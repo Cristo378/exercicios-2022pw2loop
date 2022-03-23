8c553ea0e7d6e733c6e36fb0e08d90e0bd025267
Cristo378 <cristovaohenrique157@gmail.com>
2022-03-23 17:54:25 +0000

    Exercicios

--- exercicio1.java
+++ exercicio1.java
@@ -1,18 +1,21 @@
-public class Exercicio1 {
+public class exercicio1 {
     public static void main(String[] args) {
         //Armazena a soma
-        int soma = 0
+        int soma = 0;
         //Percorre os números de 1 à 500
         for(int num = 1;num <= 500;num++) {
             //Verificar se o número é ímpar 
-            if((num % 2) ! = 0) {
+            if((num % 2) != 0) {
                 //Verificar se o número é multiplo de 3
                 if((num % 3) ==0) {
-                    System.out.println(num);
+                    //System.out.println(soma);
+                    // soma += num;
+                    soma = soma + num;
                 }
             }
             
         }
+        System.out.println(soma);
     }
 }
 
